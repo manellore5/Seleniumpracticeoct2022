7b3295d36b097369c30ff00cd2de9ada082515b4
unknown <manellore5@gmail.com>
2022-10-16 20:30:07 -0500

    test

---
+++ src/test/java/com/learnautomation/testcases/LoginTest.java
@@ -0,0 +1,36 @@
+package com.learnautomation.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.learnautomation.base.BaseClass;
+import com.learnautomation.dataProvider.DataProviders;
+import com.learnautomation.pages.Dashboard;
+import com.learnautomation.pages.LoginPage;
+
+public class LoginTest extends BaseClass
+{
+	LoginPage login;
+	Dashboard dash;
+	
+	@Test(description = "Login to application",dataProvider = "login",dataProviderClass = DataProviders.class)
+	public void loginApp(String uname,String pass)
+	{
+		login=new LoginPage(driver);
+		
+		dash=login.loginToApplication(uname,pass);
+		
+		Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"), "Login failed");
+		
+	}
+	
+	@Test(description = "Logout from application",dependsOnMethods = "loginApp")
+	public void logoutTest()
+	{
+		dash.logOut();
+		
+		Assert.assertTrue(driver.getCurrentUrl().contains("login"), "Logout failed");
+	}
+	
+
+}
